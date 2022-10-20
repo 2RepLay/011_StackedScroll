3ae0dd14c215695f5968dfe78985840e1a252262
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-20 22:05:24 +0300

    Initial Commit

---
+++ 011_StackedScroll/_11_StackedScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  _11_StackedScrollApp.swift
+//  011_StackedScroll
+//
+//  Created by nikita on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _11_StackedScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
